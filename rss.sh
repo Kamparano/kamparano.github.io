6ce8861480f79f053386c5b5a02b115a784f9e35
Kamparano <kamparano.viro@gmail.com>
2016-09-04 18:16:19 -0700

    sxangxis GNU social ligilo al propra retejo

--- rss.sh
+++ rss.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -O- 'http://feeds.soundcloud.com/users/soundcloud:users:251994336/sounds.rss' | sed -e 's/<language>en<\/language>/<language>eo<\/language>/'
+wget -O- 'http://feeds.soundcloud.com/users/soundcloud:users:251994336/sounds.rss' | sed -e 's/<language>en<\/language>/<language>eo<\/language>/; s/<link>https:\/\/quitter.se\/kp<\/link>/<link>https:\/\/kamparano.github.io<\/link>/'
