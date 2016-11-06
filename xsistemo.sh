74a1b4e96284030eeee7e2aabe0fbb5be2042c0b
Kamparano <kamparano.viro@gmail.com>
2016-11-05 19:31:50 -0700

    aldonis tagon

--- xsistemo.sh
+++ xsistemo.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed -e 's/&#x15d;/sx/g; s/&#264;/Cx/g; s/&#365;/ux/g; s/<a href="x.html">X-Sistemo<\/a>/<a href="index.html">Unikodo<\/a>/' index.html > x.html
+sed -e 's/&#x15c;/Sx/g; s/&#x15d;/sx/g; s/&#264;/Cx/g; s/&#365;/ux/g; s/<a href="x.html">X-Sistemo<\/a>/<a href="index.html">Unikodo<\/a>/' index.html > x.html
