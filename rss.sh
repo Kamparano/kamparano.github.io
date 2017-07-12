ce422aeeaa39ecaafb8d34562b6f73b4ff5a029d
Kamparano <kamparano.viro@gmail.com>
2017-07-12 15:59:13 -0700

    #!/bin/bash --> #!/bin/sh

--- rss.sh
+++ rss.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/bin/sh
 wget -O- 'http://feeds.soundcloud.com/users/soundcloud:users:251994336/sounds.rss' | sed -e 's/<language>en<\/language>/<language>eo<\/language>/; s/<link>https:\/\/quitter.se\/kp<\/link>/<link>https:\/\/kamparano.github.io<\/link>/'
